5d78e6784ad0f5ac3eddab4f3a82a12eb6e24851
vsbron <vsbron@gmail.com>
2025-09-09 13:58:21 +0300

    Set up the Logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
+import { VscCode } from "react-icons/vsc";
+
+import { Button } from "@/components/ui/button";
+
 function Logo() {
   // Returned JSX
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <VscCode className="w-8 h-8" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
