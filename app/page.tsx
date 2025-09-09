ec1d0731c0dc922cf7cecff7e33d8fbd43b848ca
vsbron <vsbron@gmail.com>
2025-09-09 13:08:42 +0300

    Install shadcn and some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
   // Returned JSX
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button
+        variant="outline"
+        size="lg"
+        className="capitalize m-8 cursor-pointer"
+      >
+        Click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
