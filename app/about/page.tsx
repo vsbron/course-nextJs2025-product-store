0f6f5b44795f655ff2505022bd02c413a3555143
vsbron <vsbron@gmail.com>
2025-09-10 11:58:32 +0300

    Set up Prisma and some test commands in the About page for creating and getting data from DB

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,26 @@
-function AboutPage() {
+import db from "@/utils/db";
+
+async function AboutPage() {
+  // Create profile in Prisma
+  const profile = await db.testProfile.create({
+    data: {
+      name: "Random name",
+    },
+  });
+
+  // Get all users from DB
+  const users = await db.testProfile.findMany();
+
   // Returned JSX
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      {users.map(({ id, name }) => (
+        <h2 key={id} className="text-2xl font-bold">
+          {name}
+        </h2>
+      ))}
+    </div>
+  );
 }
 
 export default AboutPage;
