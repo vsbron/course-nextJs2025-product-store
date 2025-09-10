1feca3f7347192a0d30e7f9e7be45c7622397f13
vsbron <vsbron@gmail.com>
2025-09-10 12:32:19 +0300

    Add more prisma files. Built DB from the Products file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,6 @@
-import db from "@/utils/db";
-
-async function AboutPage() {
-  // Create profile in Prisma
-  const profile = await db.testProfile.create({
-    data: {
-      name: "Random name",
-    },
-  });
-
-  // Get all users from DB
-  const users = await db.testProfile.findMany();
-
+function AboutPage() {
   // Returned JSX
-  return (
-    <div>
-      {users.map(({ id, name }) => (
-        <h2 key={id} className="text-2xl font-bold">
-          {name}
-        </h2>
-      ))}
-    </div>
-  );
+  return <div>AboutPage</div>;
 }
 
 export default AboutPage;
