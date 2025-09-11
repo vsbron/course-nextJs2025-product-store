9b5d3f55f103a871d9b27b3e9416d809adde05bf
vsbron <vsbron@gmail.com>
2025-09-11 11:14:40 +0300

    Set up Hero component. Add 4 images for carousel

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,28 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
   // Returned JSX
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground mb-10">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam,
+          debitis. Id aut perferendis maiores maxime magni ab quae saepe
+          corrupti!
+        </p>
+        <Button asChild size="lg">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 
 export default Hero;
