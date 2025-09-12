ba15bb8c6bce022f5f1c5d9a02b290b4a7ad20e3
vsbron <vsbron@gmail.com>
2025-09-12 17:47:14 +0300

    Set up the UserIcon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,20 @@
-function UserIcon() {
-  // Returned JSX
-  return <div>UserIcon</div>;
+import { LuUser } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function UserIcon() {
+  // Getting the ID easily
+  // const { userId } = auth();
+
+  // Get the user and his image
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  // Set the profile image based on existing user
+  return profileImage ? (
+    <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+  ) : (
+    <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
+  );
 }
 
 export default UserIcon;
