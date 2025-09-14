ed1bac880da7a07dfdf72bb398f50f4db45447c4
vsbron <vsbron@gmail.com>
2025-09-14 13:42:09 +0300

    Set up the Textarea component

--- components/form/TextAreaInput.tsx
+++ components/form/TextAreaInput.tsx
@@ -1,6 +1,30 @@
-function TextAreaInput() {
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+// Type for Text Area props
+type TextAreaInputProps = {
+  name: string;
+  labelText?: string;
+  defaultValue?: string;
+};
+
+function TextAreaInput({ name, labelText, defaultValue }: TextAreaInputProps) {
   // Returned JSX
-  return <div>TextAreaInput</div>;
+  return (
+    <div className="mb-4">
+      <Label htmlFor={name} className="mb-2 capitalize">
+        {labelText || name}
+      </Label>
+      <Textarea
+        id={name}
+        name={name}
+        className="leading-loose"
+        defaultValue={defaultValue}
+        rows={5}
+        required
+      />
+    </div>
+  );
 }
 
 export default TextAreaInput;
