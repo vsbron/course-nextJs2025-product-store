1efe308c6073a80abcae128dc6b561eb80471c59
vsbron <vsbron@gmail.com>
2025-09-14 14:10:45 +0300

    Set up the Submit Button component

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,6 +1,44 @@
-function Buttons() {
-  // Returned JSX
-  return <div>Buttons</div>;
-}
+"use client";
 
-export default Buttons;
+import { useFormStatus } from "react-dom";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { LuTrash2, LuPenSquare } from "react-icons/lu";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+
+// Type for Submit Button props and button sized
+type btnSize = "default" | "lg" | "sm";
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+// The Submit Button component
+export function SubmitButton({ className, text, size }: SubmitButtonProps) {
+  // Get the pending state of the form
+  const { pending } = useFormStatus();
+
+  // Returned JSX
+  return (
+    <Button
+      type="submit"
+      className={cn("capitalize", className)}
+      size={size}
+      disabled={pending}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
