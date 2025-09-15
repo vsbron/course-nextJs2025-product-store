9505ba6dde38fe24c6afab0d9706a9b5ce4cf286
vsbron <vsbron@gmail.com>
2025-09-15 15:17:21 +0300

    Successully implement the Delete Product action in the Admin Products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -11,7 +11,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { fetchAdminProducts } from "@/utils/actions";
+import { deleteProductAction, fetchAdminProducts } from "@/utils/actions";
 import { formatCurrency } from "@/utils/format";
 import FormContainer from "@/components/form/FormContainer";
 import { IconButton } from "@/components/form/Buttons";
@@ -51,12 +51,27 @@ async function AdminProductsPage() {
               </TableCell>
               <TableCell>{company}</TableCell>
               <TableCell>{formatCurrency(price)}</TableCell>
-              <TableCell className="flex items-center gap-x-2"></TableCell>
+              <TableCell className="flex items-center gap-x-2">
+                <Link href={`/admin/products/${productId}/edit`}>
+                  <IconButton actionType="edit" />
+                </Link>
+                <DeleteProduct productId={productId} />
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </section>
+  );
+}
+
+// Helper component for Delete icon button
+function DeleteProduct({ productId }: { productId: string }) {
+  const deleteProduct = deleteProductAction.bind(null, { productId });
+  return (
+    <FormContainer action={deleteProduct}>
+      <IconButton actionType="delete" />
+    </FormContainer>
   );
 }
 
