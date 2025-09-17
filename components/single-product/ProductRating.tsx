f535c6067bd8e380f3938f196400f2dcc4d39cab
vsbron <vsbron@gmail.com>
2025-09-17 15:17:54 +0300

    Display actual reviews data on the product page

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,17 +1,16 @@
 import { FaStar } from "react-icons/fa";
 
-function ProductRating({ productId }: { productId: string }) {
-  // Temp
-  const rating = 4.2;
-  const count = 25;
-  const className = `flex gap-1 items-center text-md mt-1 mb-4`;
-  const countValue = `(${count}) reviews`;
+import { fetchProductRating } from "@/utils/actions";
+
+async function ProductRating({ productId }: { productId: string }) {
+  // Getting the rating and the count of the reviews
+  const { rating, count } = await fetchProductRating(productId);
 
   // Returned JSX
   return (
-    <span className={className}>
+    <span className="flex gap-1 items-center text-md mt-1 mb-4">
       <FaStar className="w-3 h-3" />
-      {rating} {countValue}
+      {rating} {`(${count}) reviews`}
     </span>
   );
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -358,8 +358,30 @@ export const fetchProductReviews = async (productId: string) => {
   return reviews;
 };
 
+// Fetching the product reviews count and rating action function
+export const fetchProductRating = async (productId: string) => {
+  // Get the product rating and reviews count
+  const result = await db.review.groupBy({
+    by: ["productId"],
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: {
+      productId,
+    },
+  });
+
+  // Return the stats or 0
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
+};
+
 // TODO:
 export const fetchProductReviewsByUser = async () => {};
 export const deleteReviewAction = async () => {};
 export const findExistingReview = async () => {};
-export const fetchProductRating = async () => {};
